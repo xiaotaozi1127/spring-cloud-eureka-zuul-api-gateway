b805ced9a7b529a77f642112bb6055577e365031
taohui <htao@thoughtworks.com>
2019-07-18 12:00:48 +0800

    [taohui] add accessFilter to filter request before going to specific service

--- src/main/java/com/cloud/spring/api/gateway/ApiGatewayApplication.java
+++ src/main/java/com/cloud/spring/api/gateway/ApiGatewayApplication.java
@@ -3,10 +3,15 @@ package com.cloud.spring.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ApiGatewayApplication {
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
